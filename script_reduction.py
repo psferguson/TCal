55d92dc28c0fc83b672563afb67b89cdb42a1b4d
Peter Ferguson <petersferguson@tamu.edu>
2019-01-29 16:40:54 -0600

    windows script reduction added

--- script_reduction.py
+++ script_reduction.py
@@ -4,17 +4,15 @@ import os
 from astropy.io import fits
 import pandas as pd
 from statsmodels import robust
-path="C:\\Users\\astrolab\\Desktop\\Peter_Ferguson\\TCal\\scans\\190125\\"
-path
+path=os.getcwd()
 files=os.listdir(path)
 images_list=[i for i in files if "nm.fits" in i ]
-images_list=[i for i in files if "5nm.fits" in i]
 scan_list=[i for i in files if "scan.dat" in i ]
 lamlist=[float(i[0:3]) for i in images_list ]
 images_list=[x for _,x in sorted(zip(lamlist,images_list))]
 def im_sum_stat(image_name):
-    light=path+image_name
-    dark=path+image_name[0:len(image_name)-5]+"dark.fits"
+    light=path+"\\"+image_name
+    dark=path+"\\"+image_name[0:len(image_name)-5]+"dark.fits"
     lightimg=fits.open(light)
     darkimg=fits.open(dark)
     lightdat=lightimg[0].data
@@ -26,7 +24,7 @@ def im_sum_stat(image_name):
     darkimg.close()
     return image_name,median,mad
 def phot_sum_stat(scan_name):
-    pdiode_frame=pd.read_csv(path+scan_name)
+    pdiode_frame=pd.read_csv(path+"\\"+scan_name)
     range(int(len(pdiode_frame['Date'])/2))
     val=[]
     errl=[]
@@ -47,28 +45,28 @@ images_summary=[im_sum_stat(i) for i in images_list]
 imstats=pd.DataFrame({'name':[i[0] for i in images_summary],'median_img':[i[1] for i in images_summary],'mad_img':[i[2] for i in images_summary]})
 imstats['lam']=[float(i[0:3]) for i in imstats['name']]
 imstats['phot_mean'],imstats['phot_sd_l'],imstats['phot_sd_d'],imstats['phot_nmeas_l'],imstats['phot_nmeas_d']=phot_sum_stat(scan_list[0])
-imstats.to_csv(path+"ccd_summary_1.csv", index=False)
-import matplotlib.pyplot as plt
-plt.plot(imstats['lam'],imstats['median_img'])
-alt.Chart(imstats).mark_line().encode(
-    x='lam',
-    y='mean_img',
-    )
-x = np.arange(100)
-data = pd.DataFrame({'x': x,
-                     'sin(x)': np.sin(x / 5)})
-
-alt.Chart(data).mark_line().encode(
-    x='x',
-    y='sin(x)'
-)
-
-list=["nm.fits" in i for i in files]
-
-[m for l in files for ]
-a="nm.fits" in files[0]
-a==True
-
-"nm.fits" in files
-
-fits.open()
+imstats.to_csv(path+"\\"+"ccd_summary_1.csv", index=False)
+# import matplotlib.pyplot as plt
+# plt.plot(imstats['lam'],imstats['median_img'])
+# alt.Chart(imstats).mark_line().encode(
+#     x='lam',
+#     y='mean_img',
+#     )
+# x = np.arange(100)
+# data = pd.DataFrame({'x': x,
+#                      'sin(x)': np.sin(x / 5)})
+#
+# alt.Chart(data).mark_line().encode(
+#     x='x',
+#     y='sin(x)'
+# )
+#
+# list=["nm.fits" in i for i in files]
+#
+# [m for l in files for ]
+# a="nm.fits" in files[0]
+# a==True
+#
+# "nm.fits" in files
+#
+# fits.open()
